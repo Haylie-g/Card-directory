f817224e7f347df7deed53ebb37b44e02bb79af0
Haylie Guzman <hguzma15@stu.southtexascollege.edu>
2024-10-06 13:52:42 -0500

    fixed rounded corners and image size

--- src/components/MainCardSingle.vue
+++ src/components/MainCardSingle.vue
@@ -11,7 +11,7 @@ const username = faker.internet.userName({ firstname: firstname, lastname: lastn
 <template>
   <!-- {{ fullname }}<br /> -->
      <div class="border border-pink-400 rounded-lg shadow-lg bg-pink-200 h-full overflow-hidden">
-      <img :src="faker.image.urlLoremFlickr({category: 'desserts'})"/>
+      <img :src="faker.image.urlLoremFlickr({ width: 640, height: 480, category: 'desserts'})" class ="rounded-t-xl"/>
      <div class="p-4">
         <p class="mb-2 text-2xl font-bold text-pink-800">{{ fullname }}</p>
         <p class="mb-3 font-normal text-pink-600">{{ job_title }}</p>
